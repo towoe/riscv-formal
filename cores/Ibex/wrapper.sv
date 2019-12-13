7f6bdd2e5e6ea11cd6451daf851077f7236c1341
Tobias Wölfel <tobias.woelfel@mailbox.org>
2019-12-13 12:43:19 +0100

    [Ibex] Update interface

--- cores/Ibex/wrapper.sv
+++ cores/Ibex/wrapper.sv
@@ -1,76 +1,69 @@
 `define FORMAL_KEEP (* keep *)
 
 module rvfi_wrapper (
-	input         clock,
-	input         reset,
-	`RVFI_OUTPUTS
+      input         clock,
+      input         reset,
+      `RVFI_OUTPUTS
 );
 
-	(* keep *) wire instr_req_o;
-	(* keep *) `rvformal_rand_reg instr_gnt_i;
-	(* keep *) `rvformal_rand_reg instr_rvalid_i;
-	(* keep *) wire [31:0] instr_addr_o;
-	(* keep *) `rvformal_rand_reg [31:0] instr_rdata_i;
+(* keep *) wire instr_req_o;
+(* keep *) `rvformal_rand_reg instr_gnt_i;
+(* keep *) `rvformal_rand_reg instr_rvalid_i;
+(* keep *) wire [31:0] instr_addr_o;
+(* keep *) `rvformal_rand_reg [31:0] instr_rdata_i;
+(* keep *) wire instr_err_i;
 
-	(* keep *) wire data_req_o;
-	(* keep *) `rvformal_rand_reg data_gnt_i;
-	(* keep *) `rvformal_rand_reg data_rvalid_i;
-	(* keep *) wire data_we_o;
-	(* keep *) wire [3:0] data_be_o;
-	(* keep *) wire [31:0] data_addr_o;
-	(* keep *) wire [31:0] data_wdata_o;
-	(* keep *) `rvformal_rand_reg [31:0] data_rdata_i;
-	(* keep *) `rvformal_rand_reg data_err_i;
+(* keep *) wire data_req_o;
+(* keep *) `rvformal_rand_reg data_gnt_i;
+(* keep *) `rvformal_rand_reg data_rvalid_i;
+(* keep *) wire data_we_o;
+(* keep *) wire [3:0] data_be_o;
+(* keep *) wire [31:0] data_addr_o;
+(* keep *) wire [31:0] data_wdata_o;
+(* keep *) `rvformal_rand_reg [31:0] data_rdata_i;
+(* keep *) `rvformal_rand_reg data_err_i;
 
-	(* keep *) wire sleeping;
+(* keep *) wire sleeping;
 
-	ibex_core uut (
-		.clk_i               (clock    ),
-		.rst_ni              (!reset   ),
-        .clock_en_i          (1),
-        .test_en_i           (0),
+ibex_core uut (
+      .clk_i               (clock    ),
+      .rst_ni              (!reset   ),
 
-        .core_id_i           (4'h0),
-        .cluster_id_i        (6'h0),
-        .boot_addr_i         (32'h0),
+      .test_en_i           (0),
 
-        .instr_req_o         (instr_req_o),
-        .instr_gnt_i         (instr_gnt_i),
-        .instr_rvalid_i      (instr_rvalid_i),
-        .instr_addr_o        (instr_addr_o),
-        .instr_rdata_i       (instr_rdata_i),
+      .hart_id_i           (32'h0),
+      .boot_addr_i         (32'h0),
 
-        .data_req_o          (data_req_o),
-        .data_gnt_i          (data_gnt_i),
-        .data_rvalid_i       (data_rvalid_i),
-        .data_we_o           (data_we_o),
-        .data_be_o           (data_be_o),
-        .data_addr_o         (data_addr_o),
-        .data_wdata_o        (data_wdata_o),
-        .data_rdata_i        (data_rdata_i),
-        .data_err_i          (data_err_i),
+      .instr_req_o         (instr_req_o),
+      .instr_gnt_i         (instr_gnt_i),
+      .instr_rvalid_i      (instr_rvalid_i),
+      .instr_addr_o        (instr_addr_o),
+      .instr_rdata_i       (instr_rdata_i),
+      .instr_err_i         (instr_err_i),
 
-        .irq_i               (0),
-        .irq_id_i            (4'h0),
-        .irq_ack_o           (),
-        .irq_id_o            (),
+      .data_req_o          (data_req_o),
+      .data_gnt_i          (data_gnt_i),
+      .data_rvalid_i       (data_rvalid_i),
+      .data_we_o           (data_we_o),
+      .data_be_o           (data_be_o),
+      .data_addr_o         (data_addr_o),
+      .data_wdata_o        (data_wdata_o),
+      .data_rdata_i        (data_rdata_i),
+      .data_err_i          (data_err_i),
 
-        .debug_req_i         (0),
-        .debug_gnt_o         (),
-        .debug_rvalid_o      (),
-        .debug_addr_i        (15'hx),
-        .debug_we_i          (1'bx),
-        .debug_wdata_i       (32'hx),
-        .debug_rdata_o       (),
-        .debug_halted_o      (),
-        .debug_halt_i        (1'b0),
-        .debug_resume_i      (1'b0),
+      .irq_software_i      (1'b0),
+      .irq_timer_i         (1'b0),
+      .irq_external_i      (1'b0),
+      .irq_fast_i          (15'b0),
+      .irq_nm_i            (1'b0),
 
-        .fetch_enable_i      (1'b1),
-        .ext_perf_counters_i (),
+      .debug_req_i         ('b0),
 
-		`RVFI_CONN
-	);
+      .fetch_enable_i      ('b1),
+      .core_sleep_o        (),
+
+      `RVFI_CONN
+);
 
 endmodule
 
